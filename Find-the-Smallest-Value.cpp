0569cfeafb05f83f6e35c891dece7cbec80b86fb
samhelal <93845103+samhelal@users.noreply.github.com>
2022-04-18 06:02:40 +0200

    Add files via upload

---
+++ Find-the-Smallest-Value.cpp
@@ -0,0 +1,27 @@
+/*(Find the Smallest Value) Write an application that finds
+the smallest of several integers. Assume that the first value
+read specifies the number of values to input from the user.
+(c++ how to program)*/
+#include<iostream>
+using namespace std ;
+
+int main (){
+
+    int number(0) , smallest(0) ;
+    cout << " Enter number : ";
+    cin >> number ;
+    smallest = number ;
+    cout << " Enter number : ";
+
+    while ( cin >> number ) {
+        cout << " Enter number : ";
+
+        if (number<=smallest){
+            smallest = number ;
+        }
+
+
+    }
+    cout << " The smallest is : " << smallest << endl ;
+
+}
